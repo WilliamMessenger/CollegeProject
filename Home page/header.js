51060316f0ca9e600b320259e119a0baee4ad197
Scrypteez <joshualeach1010@gmail.com>
2021-03-16 14:05:56 +0000

    Added home page and header[in progress]

---
+++ Home page/header.js
@@ -0,0 +1,40 @@
+$(document).ready(function() {
+
+    alert("ATTENTION, see Jude's opinion below!")
+
+    $("#homeButton").click(function() {
+        $(".navButton").css("background-color", "#343a40")
+        $("#homeButton").css("background-color", "#00ADFB")
+    })
+
+    $("#aboutButton").click(function() {
+        $(".navButton").css("background-color", "#343a40")
+        $("#aboutButton").css("background-color", "#00ADFB")
+    })
+
+    $("#helpButton").click(function() {
+        $(".navButton").css("background-color", "#343a40")
+        $("#helpButton").css("background-color", "#00ADFB")
+    })
+
+    $("#staffButton").click(function() {
+        $(".navButton").css("background-color", "#343a40")
+        $("#staffButton").css("background-color", "#00ADFB")
+    })
+
+    $("#studentButton").click(function() {
+        $(".navButton").css("background-color", "#343a40")
+        $("#studentButton").css("background-color", "#00ADFB")
+    })
+
+    $("#resourceButton").click(function() {
+        $(".navButton").css("background-color", "#343a40")
+        $("#resourceButton").css("background-color", "#00ADFB")
+    })
+
+    $("#courseButton").click(function() {
+        $(".navButton").css("background-color", "#343a40")
+        $("#courseButton").css("background-color", "#00ADFB")
+    })
+
+});
