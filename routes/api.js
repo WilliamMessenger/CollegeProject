54cb05fb749cc9cb209ae6f64a733aa93e39b342
WilliamMessenger <52128126+WilliamMessenger@users.noreply.github.com>
2021-03-23 15:45:46 +0000

    Route Added

--- routes/api.js
+++ routes/api.js
@@ -4,5 +4,6 @@ const authController = require('../controllers/auth');
 
 router.post('/auth/register', authController.register);
 router.post('/auth/login', authController.login);
+router.post('/auth/verify', authController.verifyJWT, (req, res) => res.sendStatus(204));
 
 module.exports = router;
